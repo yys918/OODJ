63c5a4b895965fa5dd8c0c148ed27c43bc571ce7
yys918 <77063456+yys918@users.noreply.github.com>
2023-08-08 14:23:15 +0800

    Add files via upload

---
+++ OODJ_Assignment/src/oodj_assignment/OODJ_Assignment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oodj_assignment;
+
+/**
+ *
+ * @author yyun
+ */
+public class OODJ_Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
