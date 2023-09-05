97ecb62ee6005453b4610307d83430b90c317823
yys_9 <xinkosow@gmail.com>
2023-09-05 22:52:29 +0800

    Modify files

---
+++ OODJ_Assignment/src/oodj_assignment/Supplier.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oodj_assignment;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yyun
+ */
+public class Supplier {
+    private String ID;
+    private String name;
+    private String address;
+    private ArrayList<String> supplier = new ArrayList<String>();
+    
+    public ArrayList ViewSupplierEntry(){
+        
+    }
+    
+    public ArrayList AddSupplierEntry(){
+        
+    }
+    
+    public ArrayList EditSupplierEntry(){
+        //Delete & Edit
+    }
+}
