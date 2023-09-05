f119d51fdc78db6fdf029d3efcc5fd48cda19e2e
yys_9 <xinkosow@gmail.com>
2023-09-05 12:05:11 +0800

    Modify files

--- OODJ_Assignment/src/oodj_assignment/Item.java
+++ OODJ_Assignment/src/oodj_assignment/Item.java
@@ -4,14 +4,41 @@ package oodj_assignment;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 
 public class Item {
-    
+    private String itemCode;
+    private String itemName;
+    private String supplierID;
+    private ArrayList<String> items = new ArrayList<String>();
+    //one function for open and close file
     public Item() throws FileNotFoundException{
-    FileReader fr = new FileReader("C:\\Users\\yyun\\OneDrive - Asia Pacific University\\Documents\\Year 2\\Object Oriented Development with Java\\Assignment\\item.txt");
-    BufferedReader br = new BufferedReader(fr);
+        FileReader fr = new FileReader("C:\\Users\\yyun\\OneDrive - Asia Pacific University\\Documents\\Year 2\\Object Oriented Development with Java\\Assignment\\item.txt");
+        BufferedReader br = new BufferedReader(fr);
     }
     
+    public ArrayList ViewItemEntry() throws FileNotFoundException{
+        FileReader fr = new FileReader("C:\\Users\\yyun\\OneDrive - Asia Pacific University\\Documents\\Year 2\\Object Oriented Development with Java\\Assignment\\item.txt");
+        BufferedReader br = new BufferedReader(fr);
+        //while tokenzer has token, add line to arraylist
+        System.out.println(items);
+        return items;
+    }
     
+    public ArrayList AddItemEntry(){
+     //append to  
+     return items;
+    }
+    
+    public ArrayList EditItemEntry(){
+        //Edit
+        return items;
+    }
+    
+    public ArrayList DltItemEntry(){
+        //Delete
+        
+        return items;
+    }
 }
