7c7a807ec6283849b9de281dd4e6d3357f312e11
yys_9 <xinkosow@gmail.com>
2023-09-10 16:42:10 +0800

    a

---
+++ OODJ_Assignment/src/oodj_assignment/Employee.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oodj_assignment;
+
+/**
+ *
+ * @author yyun
+ */
+public class Employee {
+    private String name;
+    private String password;
+    private String role;
+    private String address;
+    private String email;
+    private String age;
+    private String gender;
+    
+    public Employee(String name, String role, String address, String email, String age, String gender, String password){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.role = role;
+        this.address = address;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+}
